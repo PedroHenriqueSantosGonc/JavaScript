92cd6ef80a3dd10f2a0c3b11fce59a800cda0829
Pedro Henrique dos Santos Gonçalves <167327447+PedroHenriqueSantosGonc@users.noreply.github.com>
2025-01-28 23:12:51 -0300

    Atualizei a pasta JavaScript

---
+++ Aula06/EX003/script.js
@@ -0,0 +1,7 @@
+var n1 = Number(prompt('Digite um número: '))
+var n2 = Number(prompt('Digite outro número: '))
+var s = n1 + n2
+
+alert(`A soma entre os valores ${n1} e ${n2} é de: ${s}`) //Concatenação
+// 'number + number' serve para adição
+// 'string + string' serve para concatenação
---
+++ Aula06/EX004/script.js
@@ -0,0 +1,4 @@
+var nome = prompt('Qual é seu nome?')
+document.write(`Ola, ${nome}! seu nome tem ${nome.length} letras<br>`)
+document.write(`Seu nome em minusculas é ${nome.toLowerCase()}<br>`)
+document.write(`Seu nome em maiusculas é ${nome.toUpperCase()}`)
